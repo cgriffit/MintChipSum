17fc00415bea49525f2fef4377bcf33dada9369a
cgriffit <chanson1891@gmail.com>
2018-09-10 19:31:05 -0500

    add basic lorem ipsum functionality

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
   //create an array of strings associated with ice cream
+  let icStrings = ["mint chip", "soft serve", "cookie dough", "waffle cone", "strawberry",
+  "banana split", "gelato", "a la mode", "Ben & Jerry's", "Dairy Queen", "sundae",
+  "cookies and cream", "rocky road", "peanut butter cup", "pralines and cream", "vanilla",
+  "chocolate", "super fudge brownie", "bunny tracks", "salted caramel"];
+
 
   //on enter keypress
   $("input[type='number']").keypress(function(event) {
     if(event.which === 13) {
+      let numOfParagraphs = $("#numParagraphs").val();
       //generate specified number of paragraphs of lorem ipsum
+      for (let i = 0; i < numOfParagraphs; i++) {
+        let myIpsum = "";
+        for (let j = 0; j < Math.floor((Math.random() * 100) + 10); j++) {
+          myIpsum += icStrings[ Math.floor(Math.random() * icStrings.length)] + " ";
+        }
+        //display lorem ipsum
+        $("#loremIpsum").append("<p>" + myIpsum + "</p>");
+      }
 
-      //display lorem ipsum
+      //display button for new lorem ipsum
+      $("button").removeClass("hide");
     }
   });
 
@@ -16,6 +31,7 @@ $(document).ready(function() {
 
     //clear number
       $("#numParagraphs").val("");
+
   });
 
 });
