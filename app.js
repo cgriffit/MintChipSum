4bb7097e8c71160d38cc370ee987648698257a1f
cgriffit <chanson1891@gmail.com>
2018-09-10 20:17:25 -0500

    fix basic lorem ipsum functionality

--- app.js
+++ app.js
@@ -5,33 +5,38 @@ $(document).ready(function() {
   "cookies and cream", "rocky road", "peanut butter cup", "pralines and cream", "vanilla",
   "chocolate", "super fudge brownie", "bunny tracks", "salted caramel"];
 
-
   //on enter keypress
   $("input[type='number']").keypress(function(event) {
     if(event.which === 13) {
       let numOfParagraphs = $("#numParagraphs").val();
+
       //generate specified number of paragraphs of lorem ipsum
       for (let i = 0; i < numOfParagraphs; i++) {
         let myIpsum = "";
         for (let j = 0; j < Math.floor((Math.random() * 100) + 10); j++) {
           myIpsum += icStrings[ Math.floor(Math.random() * icStrings.length)] + " ";
         }
+
         //display lorem ipsum
         $("#loremIpsum").append("<p>" + myIpsum + "</p>");
       }
 
       //display button for new lorem ipsum
       $("button").removeClass("hide");
+      
+      //stop form from submitting
+      event.preventDefault();
     }
   });
 
   //on button press
   $("button").on("click", function() {
     //remove lorem ipsum - should this be remove, hide, or clear?
-
+    $("#loremIpsum").html("");
     //clear number
-      $("#numParagraphs").val("");
-
+    $("#numParagraphs").val("").focus();
+    //hide button for new lorem ipsum
+    $("button").addClass("hide");
   });
 
 });
